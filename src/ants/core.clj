29712bf3604a3bbd117bb3154a9f99c167cbde8d
Paul Cavallaro <paulcavallaro@gmail.com>
2011-07-09 17:43:37 -0400

    Should do basic reading and updating of game state.

--- src/ants/core.clj
+++ src/ants/core.clj
@@ -15,27 +15,51 @@
 (def ants [])
 (def dead-ants [])
 (def turn 0)
+(def go false)
+(def map-state (ref nil))
+
+
+(defn rc->idx
+  ([[row col]]
+     (rc->idx row col (:cols settings)))
+  ([row col]
+     (rc->idx row col (:cols settings)))
+  ([row col num-cols]
+     (+  col (* row num-cols))))
 
 (defn parse-update
   [line]
   (let [split-line (split line #" ")
         str->int (fn [x] (Integer. x))
-        lst->ints (fn [lst1 lst2] (conj lst1
-                                  (vec
-                                   (map str->int
-                                        (rest lst2)))))]
+        lst->ints (fn [lst] (vec (map str->int lst)))]
     (case (count split-line)
+          1 (if (= (first split-line) "go")
+              (def go true))
           2 (def turn (Integer. (second split-line)))
           3 (case (first split-line)
-                  "f" (def food
-                        (lst->ints food split-line))
-                  "w" (def water
-                        (lst->ints water split-line)))
+                  "f" (let [new-food (lst->ints (rest split-line))]
+                        (def food (conj food new-food))
+                        (dosync
+                         (commute map-state
+                                  (fn [x] (assoc x (rc->idx new-food) :F)))))
+                  "w" (let [new-water (lst->ints (rest split-line))]
+                        (def water (conj water new-water))
+                        (dosync
+                         (commute map-state
+                                  (fn [x] (assoc x (rc->idx new-water) :W))))))
           4 (case (first split-line)
-                  "a" (def ants
-                        (lst->ints ants split-line))
-                  "d" (def dead-ants
-                        (lst->ints dead-ants split-line)))
+                  "a" (let [new-ant (lst->ints (rest split-line))]
+                        (def ants (conj ants new-ant))
+                        (dosync
+                         (commute map-state
+                                  (fn [x] (assoc x (rc->idx (take 2 new-ant))
+                                                (keyword (str "A" (last new-ant))))))))
+                  "d" (let [new-dead-ant (lst->ints (rest split-line))]
+                        (def dead-ants (conj dead-ants new-dead-ant))
+                        (dosync
+                         (commute map-state
+                                  (fn [x] (assoc x (rc->idx (take 2 new-dead-ant))
+                                                (keyword (str "D" (last new-dead-ant)))))))))
           nil)))
 
 (defn parse-settings
@@ -58,13 +82,7 @@
              true))))
 
 (defn make-map
-  "Creates a map consisting of a vector of row vectors
-  containing an entry for each col"
-  [rows cols]
-  (vec (repeat rows (vec (repeat cols :L)))))
-
-(defn make-long-map
-  "Creates a map consisting of land tiles"
+  "Creates a map consisting of all land tiles"
   [rows cols]
   (vec (repeat (* rows cols) :L)))
 
@@ -73,7 +91,6 @@
   read from standard input"
   []
   (def settings (parse-settings settings (read-line))))
-  
 
 (defn lookup-map
   "Look up what's at the given position in the map"
@@ -87,7 +104,9 @@
 (defn print-map
   ([]
      (print-map game-map (:rows settings) (:cols settings)))
-  ([map rows cols]
+  ([g-map]
+     (print-map g-map (:rows settings) (:cols settings)))
+  ([g-map rows cols]
      (println
       (second (reduce
                (fn [[num repr] nxt]
@@ -95,20 +114,17 @@
                    (vector 1 (str repr "\n" nxt " "))
                    (vector (+ num 1) (str repr nxt " "))))
                [0 ""]
-               game-map)))))
+               g-map)))))
 
 (defn clear-game-state []
   (def food [])
   (def water [])
   (def ants [])
-  (def dead-ants []))
+  (def dead-ants [])
+  (def go false))
 
 (defn update-game
-  ([] (update-game (java.io.BufferedReader. *in*)))
-  ([rdr]
-     (clear-game-state)
-     (doseq [line (line-seq rdr)]
-       (parse-update line))))
+  [] (parse-update (read-line)))
 
 (defn get-ants [player]
   (filter (fn [x] (= player (nth x 2))) ants))
@@ -116,8 +132,21 @@
 (defn my-ants []
   (get-ants 0))
 
+(defn end-turn []
+  (def go false)
+  (println "go")
+  (flush))
+
 (defn -main []
   (while (not (:ready settings))
     (init-game))
-  (def game-map (make-long-map (:rows settings) (:cols settings)))
-  (print-map))
+  (def game-map (make-map (:rows settings) (:cols settings)))
+  (dosync
+   (ref-set map-state game-map))
+  (while true
+    (println (str "turn is " turn))
+    (print-map @map-state)
+    (while (not go)
+      (update-game))
+    (println (str "turn is " turn))
+    (end-turn)))
